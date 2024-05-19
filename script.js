c570cc4e907a858038179ea6be91e4187a2cb3ba
Jonathan Viray <jfviray7@gmail.com>
2024-05-19 16:20:41 -0500

    Change delete-list-btn selector to class

--- script.js
+++ script.js
@@ -69,9 +69,9 @@ const addNewList = (parentEl, title) => {
   headerDiv.appendChild(titleInput);
 
   const deleteListBtn = document.createElement('button');
-  deleteListBtn.setAttribute('id', 'delete-list-btn');
   deleteListBtn.setAttribute('type', 'button');
-  deleteListBtn.className = 'p-1.5 rounded-md hover:bg-[#091e420f]';
+  deleteListBtn.className =
+    'delete-list-btn p-1.5 rounded-md hover:bg-[#091e420f]';
   deleteListBtn.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>';
 
@@ -251,7 +251,7 @@ onClick.bind(activeLists)((e) => {
   }
 
   // Handle delete list via button
-  if (e.target.closest('button')?.id === 'delete-list-btn') {
+  if (e.target.closest('button')?.classList.contains('delete-list-btn')) {
     const list = e.target.closest('li');
     activeLists.removeChild(list);
 
